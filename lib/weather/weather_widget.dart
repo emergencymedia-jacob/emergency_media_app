8c7f26202ecae5b8af0355fe7612a484435015b2
emergencymedia-jacob <163681979+emergencymedia-jacob@users.noreply.github.com>
2024-03-16 15:28:42 -0500

    Add files via upload

--- lib/weather/weather_widget.dart
+++ lib/weather/weather_widget.dart
@@ -1,14 +1,9 @@
-import '/flutter_flow/flutter_flow_theme.dart';
-import '/flutter_flow/flutter_flow_util.dart';
-import '/flutter_flow/flutter_flow_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 import 'weather_model.dart';
-export 'weather_model.dart';
+import 'weather_radar.dart'; // Import the weather radar widget
 
 class WeatherWidget extends StatefulWidget {
-  const WeatherWidget({super.key});
+  const WeatherWidget({Key? key}) : super(key: key);
 
   @override
   State<WeatherWidget> createState() => _WeatherWidgetState();
@@ -23,15 +18,16 @@ class _WeatherWidgetState extends State<WeatherWidget>
   @override
   void initState() {
     super.initState();
-    _model = createModel(context, () => WeatherModel());
+    _model = createModel(context, () => WeatherModel()); // You need to define createModel function
 
     _model.tabBarController = TabController(
       vsync: this,
       length: 3,
       initialIndex: 0,
     )..addListener(() => setState(() {}));
-    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+    WidgetsBinding.instance!.addPostFrameCallback((_) => setState(() {}));
   }
+  
 
   @override
   void dispose() {
@@ -59,21 +55,21 @@ class _WeatherWidgetState extends State<WeatherWidget>
                 child: Column(
                   children: [
                     Align(
-                      alignment: Alignment(0.0, 0),
+                      alignment: const Alignment(0.0, 0),
                       child: TabBar(
                         labelColor: FlutterFlowTheme.of(context).primaryText,
                         unselectedLabelColor:
                             FlutterFlowTheme.of(context).secondaryText,
                         labelStyle: FlutterFlowTheme.of(context).labelSmall,
-                        unselectedLabelStyle: TextStyle(),
+                        unselectedLabelStyle: const TextStyle(),
                         indicatorColor: FlutterFlowTheme.of(context).primary,
-                        padding: EdgeInsets.all(4.0),
+                        padding: const EdgeInsets.all(4.0),
                         tabs: [
                           Tab(
                             text: FFLocalizations.of(context).getText(
                               'gym662u5' /* Radar */,
                             ),
-                            icon: Icon(
+                            icon: const Icon(
                               Icons.radar,
                             ),
                           ),
@@ -81,7 +77,7 @@ class _WeatherWidgetState extends State<WeatherWidget>
                             text: FFLocalizations.of(context).getText(
                               'bnncmdw5' /* Forecast */,
                             ),
-                            icon: Icon(
+                            icon: const Icon(
                               Icons.cloudy_snowing,
                             ),
                           ),
@@ -89,7 +85,7 @@ class _WeatherWidgetState extends State<WeatherWidget>
                             text: FFLocalizations.of(context).getText(
                               'ppadbwi3' /* Outlooks */,
                             ),
-                            icon: Icon(
+                            icon: const Icon(
                               Icons.thunderstorm_rounded,
                             ),
                           ),
@@ -104,6 +100,7 @@ class _WeatherWidgetState extends State<WeatherWidget>
                       child: TabBarView(
                         controller: _model.tabBarController,
                         children: [
+                          WeatherRadarWidget(), // Weather radar on Radar tab
                           Container(
                             width: 100.0,
                             height: 100.0,
@@ -111,63 +108,14 @@ class _WeatherWidgetState extends State<WeatherWidget>
                               color: FlutterFlowTheme.of(context)
                                   .secondaryBackground,
                             ),
-                          ),
-                          SingleChildScrollView(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.max,
-                              children: [
-                                ListView(
-                                  padding: EdgeInsets.zero,
-                                  shrinkWrap: true,
-                                  scrollDirection: Axis.vertical,
-                                  children: [
-                                    Container(
-                                      width: double.infinity,
-                                      decoration: BoxDecoration(
-                                        color: FlutterFlowTheme.of(context)
-                                            .secondaryBackground,
-                                      ),
-                                    ),
-                                    Container(
-                                      width: double.infinity,
-                                      decoration: BoxDecoration(
-                                        color: FlutterFlowTheme.of(context)
-                                            .secondaryBackground,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
+                          ), // Placeholder for Forecast tab
+                          Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              color: FlutterFlowTheme.of(context)
+                                  .secondaryBackground,
                             ),
-                          ),
-                          SingleChildScrollView(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.max,
-                              children: [
-                                ListView(
-                                  padding: EdgeInsets.zero,
-                                  shrinkWrap: true,
-                                  scrollDirection: Axis.vertical,
-                                  children: [
-                                    Container(
-                                      width: double.infinity,
-                                      decoration: BoxDecoration(
-                                        color: FlutterFlowTheme.of(context)
-                                            .secondaryBackground,
-                                      ),
-                                    ),
-                                    Container(
-                                      width: double.infinity,
-                                      decoration: BoxDecoration(
-                                        color: FlutterFlowTheme.of(context)
-                                            .secondaryBackground,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
+                          ), // Placeholder for Outlooks tab
                         ],
                       ),
                     ),
